5c8046983817c288a9ad33c78c3e65f74092a5c6
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2024-12-12 22:58:18 +0900

    tutorial for matrix

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+// 行列を表示
+func matPrint(X mat.Matrix) {
+	fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
+	fmt.Printf("%v\n", fa)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	a := mat.NewDense(3, 3, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	b := mat.NewDense(3, 1, []float64{1, 2, 3})
+	c := mat.NewDense(3, 1, nil)
+	c.Product(a, b)
+	matPrint(a)
+	matPrint(b)
+	matPrint(c)
 }
