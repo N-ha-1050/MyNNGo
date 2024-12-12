a3aa8d9c6af758170dbf44a1409498d93471e2f8
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2024-12-13 00:04:37 +0900

    4.6.1 単一のニューロンを実装

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"math"
+	"os"
 
 	"gonum.org/v1/gonum/mat"
 )
@@ -12,12 +17,61 @@ func matPrint(X mat.Matrix) {
 	fmt.Printf("%v\n", fa)
 }
 
+// 各要素の値が0-1の行列をグレースケールでJpeg画像に保存
+func matSaveGrayImage(X mat.Matrix, name string) {
+	r, c := X.Dims()
+	img := image.NewGray(image.Rect(0, 0, c, r))
+	for i := 0; i < c; i++ {
+		for j := 0; j < r; j++ {
+			img.SetGray(i, j, color.Gray{Y: uint8(X.At(j, i) * 256)})
+		}
+	}
+	file, _ := os.Create(name)
+	jpeg.Encode(file, img, &jpeg.Options{Quality: 100})
+}
+
 func main() {
-	a := mat.NewDense(3, 3, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
-	b := mat.NewDense(3, 1, []float64{1, 2, 3})
-	c := mat.NewDense(3, 1, nil)
-	c.Product(a, b)
-	matPrint(a)
-	matPrint(b)
-	matPrint(c)
+
+	// x, y座標
+	xData := make([]float64, 10)
+	xValue := -1.0
+	for i := 0; i < 10; i++ {
+		xData[i] = xValue
+		xValue += 0.2
+	}
+
+	yData := make([]float64, 10)
+	yValue := -1.0
+	for i := 0; i < 10; i++ {
+		yData[i] = yValue
+		yValue += 0.2
+	}
+
+	x := mat.NewVecDense(10, xData)
+	y := mat.NewVecDense(10, yData)
+
+	z := mat.NewDense(10, 10, nil) // 出力を格納する10x10のグリッド
+
+	w_x := 2.5 // x座標の入力の重み
+	w_y := 3.0 // y座標の入力の重み
+
+	bias := 0.1 // バイアス
+
+	// グリッドの各マスでニューロンの演算
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+
+			// 入力と重みの積の総和 + バイアス
+			u := x.At(i, 0)*w_x + y.At(j, 0)*w_y + bias
+
+			// グリッドに出力を格納
+			zValue := 1 / (1 + math.Exp(-u))
+			z.Set(j, i, zValue)
+		}
+	}
+
+	matPrint(z)
+
+	// グリッドの表示
+	matSaveGrayImage(z, "sample.jpg")
 }
